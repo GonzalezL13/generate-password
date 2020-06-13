60b9da303ba15030744442856468945659abf1ab
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-13 15:23:39 -0700

    array of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,92 @@
 // Assignment code here
+// Assignment code here
+//array of lower case character
+var lowerCaseCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+//array of upper case characters
+var upperCaseCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+//array of number characters
+var numericCharacters = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0"
+];
+//array of special characters
+var specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "~",
+  "<",
+  ">",
+  ";",
+  "/"
+];
 
 
 // Get references to the #generate element
